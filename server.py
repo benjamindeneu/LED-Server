8cf4f5fc7811a1816a76365312757f618be048aa
simondankelmann <simon.dankelmann@gmail.com>
2017-01-02 17:02:38 +0000

    fixed issues with sockets

--- server.py
+++ server.py
@@ -1,28 +1,62 @@
-from config import *
-from led_functions import *
-from process_functions import *
+import select
+import socket
+import pigpio
 
-#START SOCKET
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((SERVER_IP, SERVER_PORT))
-#START LISTENING, MAX 5 CLIENTS
-serversocket.listen(5)
+#NETWORK SETTINGS
+SERVER_IP = ""
+SERVER_PORT = 12345
 
+#IO-PIN SETUP
+RED_PIN   = 17
+GREEN_PIN = 22
+BLUE_PIN  = 24
+
+#PIGPIO OBJECT TO SWITCH/DIM LED CHANNELS
+pi = pigpio.pi()
+
+#MAXIMUM OF BRIGHTNESS
+MAX_BRIGHTNESS = 255
+
+led_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+led_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+led_socket.bind((SERVER_IP, SERVER_PORT))
+led_socket.listen(5)
+
+print "LED SERVER STARTED AT " + SERVER_IP + " ON PORT: " + str(SERVER_PORT)
+
+def setLights(pin, brightness):
+        realBrightness = int(int(brightness) * (float(MAX_BRIGHTNESS) / 255.0))
+        pi.set_PWM_dutycycle(pin, realBrightness)
+
+def setRGB(r,g,b):
+        setLights(RED_PIN, r)
+        setLights(GREEN_PIN, g)
+        setLights(BLUE_PIN,b)
+
+
+def setRGB(r,g,b):
+        setLights(RED_PIN, r)
+        setLights(GREEN_PIN, g)
+        setLights(BLUE_PIN,b)
+
+read_list = [led_socket]
 while True:
-    connection, address = serversocket.accept()
-    buf = connection.recv(32)
-    if len(buf) > 0:
-
-        #COPS EFFECT
-        if buf == "COPS":
-                killProcess(iCurrentPid)
-                iCurrentPid = startProcess("python ./cops.py")
-
-        aRGB = buf.split(',')
-        if len(aRGB) == 3:
-                killProcess(iCurrentPid)
-                r = int(aRGB[0])
-                g = int(aRGB[1])
-                b = int(aRGB[2])
-                setRGB(r,g,b)
+    readable, writable, errored = select.select(read_list, [], [])
+    for s in readable:
+        if s is led_socket:
+            client_socket, address = led_socket.accept()
+            read_list.append(client_socket)
+            #print "Connection from", address
+        else:
+            data = s.recv(64)
+            if data:
+                aRGB = data.split(',')
+                if len(aRGB) == 3:
+                        r = int(aRGB[0])
+                        g = int(aRGB[1])
+                        b = int(aRGB[2])
+                        setRGB(r,g,b)
+            else:
+                s.close()
+                read_list.remove(s)
 
